244df4b8619d7b07433da2d50b6e2a8b41063371
Luis Daniel Vargas Rodríguez <136190060+LuisDaniel-byte@users.noreply.github.com>
2023-07-10 23:19:58 -0600

    first commit

---
+++ app/src/main/java/com/example/apiapp/ApiService.kt
@@ -0,0 +1,11 @@
+package com.example.apiapp
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+
+        @POST("login")
+        fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
+    }
